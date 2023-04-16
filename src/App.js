92506a342f7e4b158a5e3c6b7bddf3ab5b4a591c
kaylaraniahasna <kaylaraniahasna20@gmail.com>
2023-04-16 13:34:18 +0700

    commit kayy

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Halaman_Account from './components/halaman_account/account';
 import Desc from './components/halaman_desc/desc';
 import Halaman_Home_Guest from './components/halaman_home_guest/home_guest';
 import Halaman_Login from './components/halaman_login/login';
+import Halaman_Keranjang from './components/halaman_keranjang/keranjang';
 import Test from './components/tes';
 
 function App() {
   return (
     
     <div>
-      {/* <Desc /> */}
+      {<Desc />}
       {/* <Halaman_Home_Guest /> */}
       {/* <Halaman_Login /> */}
+      {/* <Halaman_Keranjang /> */}
       {/* <Test /> */}
 
     </div>
