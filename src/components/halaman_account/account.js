13767e6f526964eefc2fc8a153e2514e93ddd1c1
Widya <lalunadelavonte@gmail.com>
2023-04-17 12:12:06 +0700

    mengupdate account.js dan tabslide.js by widya

--- src/components/halaman_account/account.js
+++ src/components/halaman_account/account.js
@@ -10,12 +10,14 @@ function Halaman_Account() {
 	  <div>
         <Bar />
         <div className='container flex flex-row'>
-          <div className='basis-2/3 mx-auto w-1/5 h-screen bg-light-cream my-2 rounded-md p-0' id='kotak-top'>
+          <div className='basis-2/3 mx-auto w-1/5 h-screen bg-light-cream my-6 rounded-md p-0' id='kotak-top'>
            <div className='flex'>
-            <img src={Pp} className='w-24 rounded-full m-3'/>
-           <div className='py-9'>
+            <img src={Pp} className='w-36 border-white border-2 rounded-full m-3'/>
+           <div className='py-8'>
             <h1 className='font-barlow font-bold text-lg'>Delavonte</h1>
             <h1 className='font-barlow text-sm'>Lalunadelavonte@gmail.com</h1>
+            <h1 className='font-barlow text-sm'>Member Silver</h1>
+            <button className="h-7 w-20 mt-3 bg-dark-cream  hover:bg-brown-cream font-barlow text-gray-900 text-xs rounded-full">Logout</button>
            </div>
            <div className='py-8 px-7 ml-auto'>
             <button className="h-8 w-24 m-3 bg-dark-cream  hover:bg-brown-cream font-barlow text-gray-900 text-xs rounded-full">Message</button>
@@ -25,7 +27,7 @@ function Halaman_Account() {
            {/* <div class="border-t border-gray-300 my-1"></div> */}
            <Tabslide/>
           </div>
-          <div className='basis-1/3 my-2 -mr-16 mx-auto rounded-sm bg-light-cream h-screen'>
+          <div className='basis-1/4 my-2  mx-auto rounded-sm bg-light-cream h-screen'>
             <p>ppppp</p>
           </div>
         </div>
--- src/components/halaman_account/tabslide.js
+++ src/components/halaman_account/tabslide.js
@@ -1,5 +1,6 @@
 import '../../../src/App.css';
 import '../../../src/index.css';
+import Pp from '../../assets/pp.png';
 import { useState } from 'react';
 
 const Test = () => {
@@ -9,7 +10,7 @@ const Test = () => {
     return (
       <div className="bg">
         <div className="max-w-6xl mx-auto ">
-          <div className="border-b-2 border-gray-200">
+          <div className="border-0 rounded">
             <nav className="-mb-px flex">
               {tabs.map((tab, index) => (
                 <button
@@ -34,8 +35,42 @@ const Test = () => {
                   activeTab === index ? 'block' : 'hidden'
                 }`}
               >
-                <h2>{tab}</h2>
-                <p>Content for {tab}</p>
+                {tab === 'Biodata' &&
+                    <div className='container mx-3 flex'>
+                        <div class="w-52 -mt-5 bg-white border border-gray-200 rounded-lg shadow">
+                            <a href="#">
+                                <img class="w-44 mx-auto my-4"  src={Pp} alt="" />
+                            </a>
+                            <div class="p-3">
+                                <a href="#" class="inline-flex items-center p-2 center  text-sm font-medium text-center text-white bg-dark-cream rounded-lg hover:bg-brown-cream focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                    Ubah Gambar
+                                    {/* <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
+                                </a>
+                                {/* <a href="#">
+                                    <h5 class="mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                </a> */}
+                                {/* <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Read more
+                                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </a> */}
+                            </div>
+                        </div>
+                    </div>
+                }
+                {tab === 'Pemesanan' &&
+                    <p>Daftar pemesanan Anda:</p>
+                }
+                {tab === 'Pengaturan' &&
+                    <div>
+                    <h3>Pengaturan akun</h3>
+                    <ul>
+                        <li>Email: johndoe@example.com</li>
+                        <li>Password: **********</li>
+                        <li>Notifikasi: ON</li>
+                    </ul>
+                    </div>
+                }
               </div>
             ))}
           </div>
