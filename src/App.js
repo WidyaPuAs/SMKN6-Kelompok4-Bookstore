2610c5b4a34362761b372589c0fe8dbac432c8c3
Widya <lalunadelavonte@gmail.com>
2023-04-18 10:10:08 +0700

    perubahan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Desc from './components/halaman_desc/desc';
+import Halaman_Account from './components/halaman_account/account'
 import Halaman_Home_Guest from './components/halaman_home_guest/home_guest';
 import Halaman_Login from './components/halaman_login/login';
+import Halaman_Keranjang from './components/halaman_keranjang/keranjang'
 
 function App() {
   return (
     <div>
       {/* <Halaman_Home_Guest /> */}
-      {/* <Halaman_Pembelian /> */}
+      {/* <Halaman_Keranjang /> */}
       <Halaman_Account/>
     </div>
   );
--- src/components/halaman_account/tabslide.js
+++ src/components/halaman_account/tabslide.js
@@ -36,26 +36,64 @@ const Test = () => {
                 }`}
               >
                 {tab === 'Biodata' &&
-                    <div className='container mx-3 flex'>
+                    <div className='container mx-5 mt-2 flex'>
                         <div class="w-52 -mt-5 bg-white border border-gray-200 rounded-lg shadow">
                             <a href="#">
                                 <img class="w-44 mx-auto my-4"  src={Pp} alt="" />
                             </a>
-                                <a href="#" class="mx-12 p-2 text-sm font-medium text-center text-white bg-dark-cream rounded-lg hover:bg-brown-cream focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                <a href="#" class="p-2 mx-12 text-sm font-medium text-center text-white bg-dark-cream rounded-lg hover:bg-brown-cream focus:ring-4 focus:outline-none focus:ring-blue-300">
                                     Ubah Gambar
                                     {/* <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
                                 </a>
                             <div class="p-3">
-                                {/* <a href="#">
-                                    <h5 class="mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                                </a> */}
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                </a>
-                            </div>
+                                <p class="my-3 font-normal text-xs text-gray-700 dark:text-gray-400">Besar file maxsimum 10mb. Ekstensi file yang diperbolehkan: .jpg .jpeg .png</p>
+                            </div>                      
                         </div>
+                        <div class="ml-6 text-gray-700">
+                          <p className='text-4xl font-bold ml-3 -mt-7 mb-3'>About</p>
+                              <div class="grid md:grid-cols-1 text-sm">
+                                  <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Username</div>
+                                      <div class="px-4 py-2">LalunaDelavonte</div>
+                                  </div>
+                                  <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Email</div>
+                                      <div class="px-4 py-2">
+                                          <a class="text-blue-800" href="mailto:laluna@example.com">laluna@example.com</a>
+                                      </div>
+                                  </div>
+                                  <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Gender</div>
+                                      <div class="px-4 py-2">Female</div>
+                                  </div>
+                                  <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Contact No.</div>
+                                      <div class="px-4 py-2">+62083495242994</div>
+                                  </div>
+                                  <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Country</div>
+                                      <div class="px-4 py-2">Indonesia</div>
+                                  </div>
+                                  {/* <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Permanant Address</div>
+                                      <div class="px-4 py-2">Arlington Heights, IL, Illinois</div>
+                                  </div>
+                                  <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Email.</div>
+                                      <div class="px-4 py-2">
+                                          <a class="text-blue-800" href="mailto:jane@example.com">jane@example.com</a>
+                                      </div>
+                                  </div>
+                                  <div class="grid grid-cols-2">
+                                      <div class="px-4 py-2 font-semibold">Birthday</div>
+                                      <div class="px-4 py-2">Feb 06, 1998</div>
+                                  </div> */}
+                                  <a href="#" class="p-2 m-3 w-36 text-sm font-medium text-center text-white bg-dark-cream rounded-lg hover:bg-brown-cream focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                    Edit Biodata
+                                    {/* <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
+                                 </a>
+                              </div>
+                          </div>
                     </div>
                 }
                 {tab === 'Pemesanan' &&
