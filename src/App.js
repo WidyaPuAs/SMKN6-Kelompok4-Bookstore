32181d7914bf7d49fcd54ebc43d882fb43e1886a
kaylaraniahasna <kaylaraniahasna20@gmail.com>
2023-04-18 10:56:45 +0700

    mengubah

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Home from './pages/home';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Buku_Terlaris from './pages/keranjang';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Halaman_Keranjang from './components/halaman_keranjang/keranjang';
 import Halaman_Account from './components/halaman_account/account'
 import Halaman_Chat from './components/halaman_chat/chat';
+import Halaman_Home_Guest from './components/halaman_home_guest/home_guest';
 
 function App() {
   return (
@@ -20,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;
