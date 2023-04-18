f1cb812f97b8afa360e83578623cd70c3d28f5cc
Widya <lalunadelavonte@gmail.com>
2023-04-18 10:57:49 +0700

    mengubah file by widya

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Home from './pages/home';
+import Home from './pages/Home';
 import Buku_Terlaris from './pages/keranjang';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Halaman_Keranjang from './components/halaman_keranjang/keranjang';
 import Halaman_Account from './components/halaman_account/account'
 import Halaman_Chat from './components/halaman_chat/chat';
+import Halaman_Home_Guest from './components/halaman_home_guest/home_guest';
 
 function App() {
   return (
