cda69afb5bf86afff756b76829a5a9f73b9d3b89
kaylaraniahasna <kaylaraniahasna20@gmail.com>
2023-04-24 21:10:43 +0700

    yaaa

--- src/components/halaman_account/tabslide.js
+++ src/components/halaman_account/tabslide.js
@@ -1,6 +1,11 @@
 import '../../../src/App.css';
 import '../../../src/index.css';
 import Pp from '../../assets/pp.png';
+import language from '../../assets/language.png';
+import pallete from '../../assets/pallete.png';
+import notifikasi from '../../assets/notifikasi.png';
+import about from '../../assets/about.png';
+import help from '../../assets/help.png';
 import { useState } from 'react';
 
 const Test = () => {
@@ -98,11 +103,102 @@ const Test = () => {
                 }
                 {tab === 'Pemesanan' &&
                     <p>Daftar pemesanan Anda:</p>
+                    
                 }
+
                 {tab === 'Pengaturan' &&
-                    <div class="ml-6 text-gray-700">
-                      <p className='text-4xl font-bold ml-3 mb-3'></p>
-                    </div>
+                <div className='pengaturan'>
+                  <div class="w-auto ml-5 mr-5 -mt-2 bg-white border border-gray-200 rounded-lg shadow">
+                    <div class="flex justify-between items-center px-3 py-3">
+                      <span class="text-gray-700">
+                        <img src={language} alt="Bahasa" class="w-5 h-5 mr-5 inline-block"/> 
+                          Bahasa
+                      </span>
+
+                        <div className="flex justify-between relative">
+                          <select className="text-sm block appearance-none  bg-white border border-gray-400 hover:border-gray-500 px-5 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                            <option>Indonesia</option>
+                            <option>Inggris</option>
+                            <option>Mandarin</option>
+                            <option>Hindi</option>
+                            <option>Spanyol</option>
+                            <option>Perancis</option>
+                            <option>Arab</option>
+                            <option>Bengali</option>
+                            <option>Rusia</option>
+                          </select>
+
+                          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg className="fill-current h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/></svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div> 
+
+
+                   {/* TAMPILAN */}
+
+                    <div class="w-auto ml-5 mr-5 mt-3 bg-white border border-gray-200 rounded-lg shadow">
+                    <div class="flex justify-between items-center px-3 py-3">
+                      <span class="text-gray-700">
+                        <img src={pallete} alt="Tampilan" class="w-5 h-5 mr-5 inline-block"/> 
+                          Tampilan
+                      </span>
+
+                        <div className="flex justify-between relative">
+                          <select className="text-sm block appearance-none  bg-white border border-gray-400 hover:border-gray-500 px-5 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                            <option>Light</option>
+                            <option>Dark</option>
+                          </select>
+
+                          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg className="fill-current h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/></svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div> 
+
+
+                   {/* NOTIFIKASI */}
+
+                    <div class="w-auto ml-5 mr-5 mt-3 bg-white border border-gray-200 rounded-lg shadow">
+                      <div class="flex justify-between items-center px-3 py-4">
+                        <span class="text-gray-700">
+                          <img src={notifikasi} alt="Notifikasi" class="w-5 h-5 mr-5 inline-block"/> 
+                            Notifikasi
+                        </span>
+
+                        <label class="relative inline-flex items-center cursor-pointer mr-2">
+                        <input type="checkbox" value="" class="sr-only peer"></input>
+                          <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                        </label>
+                      </div>
+                    </div> 
+
+
+                    {/* HELP */}
+
+                    <div class="w-auto ml-5 mr-5 mt-3 bg-white border border-gray-200 rounded-lg shadow">
+                      <div class="flex justify-between items-center px-3 py-4">
+                        <span class="text-gray-700">
+                          <img src={help} alt="Help" class="w-5 h-5 mr-5 inline-block"/> 
+                            Help
+                        </span>
+                      </div>
+                    </div> 
+
+
+                    {/* ABOUT DEVELOPMENT */}
+
+                    <div class="w-auto ml-5 mr-5 mt-3 bg-white border border-gray-200 rounded-lg shadow">
+                      <div class="flex justify-between items-center px-3 py-4">
+                        <span class="text-gray-700">
+                          <img src={about} alt="Aboutdevelopment" class="w-5 h-5 mr-5 inline-block"/> 
+                            About Development
+                        </span>
+                      </div>
+                    </div> 
+                  </div>
                 }
               </div>
             ))}
