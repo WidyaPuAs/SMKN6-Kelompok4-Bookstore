62f464c1f3c0b99e8c161a94ddb14ec674822ebc
Widya <lalunadelavonte@gmail.com>
2023-04-26 11:30:24 +0700

    mengubah app.js dan navbar by widya

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -21,10 +21,6 @@ function Bar() {
     			<img src={logoBook} alt='Logo' className='w-10 h-10 mr-2'/>
     			<span className='font-bold text-xl'>Bookstore</span>
   			</div>
-		
-			<div className='flex items-center'>
-				
-			</div>
 
 			{/* Search Bar */}
 			<div className='flex-1 ml-10'>
@@ -63,7 +59,7 @@ function Bar() {
 				{/* <a>
 				<img src={Menu} className='h-6 w-6 block sm:hidden' />
     			</a> */}
-				<button className='relative justify-center items-center focus:outline-none focus:ring group block sm:hidden'>
+				<button className='relative flex justify-center items-center focus:outline-none focus:ring group'>
 					<img src={Menu} className='h-5 w-5' />
 					<div className='absolute hidden group-focus:block top-full min-w-full w-screen bg-light-cream shadow-md mt-1 rounded'>
 						<ul className='text-left border rounded'>
