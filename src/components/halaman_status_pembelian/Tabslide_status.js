3878281a6ac71d2d4f4b9375315a62507cf9a269
Mini Project <126549135+WidyaPuAs@users.noreply.github.com>
2023-04-29 20:50:24 +0700

    Update tabslide status by widya

--- src/components/halaman_status_pembelian/Tabslide_status.js
+++ src/components/halaman_status_pembelian/Tabslide_status.js
@@ -1,1 +1,73 @@
+import '../../../src/App.css';
+import '../../../src/index.css';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+const Status = () => {
+    const [activeTab, setActiveTab] = useState(0);
+    const tabs = ['belum dibayar','sedang dikemas', 'telah dikirim', 'selesai', 'dibatalkan'];
+    const navigate = useNavigate()
+  
+    return (
+      <div className="bg">
+        <div className="max-w-6xl mx-auto ">
+          <div className="border-0 rounded">
+            <nav className="-mb-px flex">
+              {tabs.map((tab, index) => (
+                <button
+                  key={index}
+                  className={`${
+                    activeTab === index
+                      ? 'border-b-2 border-dark-cream'
+                      : 'border-b border-transparent'
+                  } text-gray-500 py-4 px-6 hover:text-dark-cream focus:outline-none`}
+                  onClick={() => setActiveTab(index)}
+                >
+                  {tab}
+                </button>
+              ))}
+            </nav>
+          </div>
+          <div className="py-8">
+            {tabs.map((tab, index) => (
+              <div
+                key={index}
+                className={`${
+                  activeTab === index ? 'block' : 'hidden'
+                }`}
+              >
+                {tab === 'belum dibayar' &&
+                    <div className='container mx-5 mt-2 flex'>
+                        
+                    </div>
+                }
+                {tab === 'Sedang dikemas' &&
+                    <div className='container mx-5 mt-2 flex'>
+                        
+                    </div>
+                }
+                {tab === 'telah dikirim' &&
+                    <div className='container mx-5 mt-2 flex'>
+                        
+                    </div>
+                }
+                {tab === 'selesai' &&
+                    <div className='container mx-5 mt-2 flex'>
+                        
+                    </div>
+                }
+                {tab === 'dibatalkan' &&
+                    <div className='container mx-5 mt-2 flex'>
+                        
+                    </div>
+                }
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Status;
 
