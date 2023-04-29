27dffafab5e493b7686328df4b9414da04f8ce1e
Mini Project <126549135+WidyaPuAs@users.noreply.github.com>
2023-04-29 20:53:12 +0700

    Update isi halaman status pembelian by widya

--- src/components/halaman_status_pembelian/status_pembelian.js
+++ src/components/halaman_status_pembelian/status_pembelian.js
@@ -1,13 +1,14 @@
 import '../../../src/App.css';
 import '../../../src/index.css';
 import Bar from '../navbar/navbar';
+import Tabslide from '../tabslide_status.js';
 
 function Halaman_Status_Pembelian() {
   return (
    <div className=''>
     <Bar />
     <div classname=''>
-      
+      <Tabslide />
     </div> 
    </div>
   );
