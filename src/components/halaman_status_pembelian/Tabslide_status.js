73ca64bab5ece58cb3ac84760280df1735873501
Widya <lalunadelavonte@gmail.com>
2023-05-04 12:22:00 +0700

    perubahan

--- src/components/halaman_status_pembelian/Tabslide_status.js
+++ src/components/halaman_status_pembelian/Tabslide_status.js
@@ -2,6 +2,7 @@ import '../../../src/App.css';
 import '../../../src/index.css';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import Pp from '../../assets/books.png'
 
 const Status = () => {
     const [activeTab, setActiveTab] = useState(0);
@@ -54,7 +55,21 @@ const Status = () => {
                 {tab === 'selesai' &&
                     <div className='ml-3 container -mt-2'>
                         <input type='text' className='w-11/12 mx-auto border border-gray-300 px-4 py-2 rounded-r-md' placeholder='Cari berdasarkan Nama Penjual, No. Pesanan atau Nama produk dalam semua pesanan'/>
-                        <div className='mt-3 bg-white rounded-md border'></div>
+                        <div className='mt-3 bg-white rounded-md border'>
+                          <div className='flex p-3 items-center'>
+                            <img className='w-8' src={Pp}/>
+                            <h1 className='font-semibold ml-2 text-sm'>Toko buku</h1>
+                            <a href="#" class="p-1 px-2 text-xs font-medium text-center text-white bg-dark-cream rounded-lg hover:bg-brown-cream focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                    chat
+                                    {/* <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
+                            </a>
+                            <a href="#" class="p-1 px-2 text-xs font-medium text-center text-white bg-dark-cream rounded-lg hover:bg-brown-cream focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                    Kunjungi toko
+                                    {/* <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
+                            </a>
+                          </div>
+
+                        </div>
                     </div>
                 }
                 {tab === 'dibatalkan' &&
