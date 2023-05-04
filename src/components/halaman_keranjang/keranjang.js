4d82d78ffe486ed4a960ff85c971ab19dadc8677
kaylaraniahasna <kaylaraniahasna20@gmail.com>
2023-05-04 09:58:51 +0700

    perubahan

--- src/components/halaman_keranjang/keranjang.js
+++ src/components/halaman_keranjang/keranjang.js
@@ -1,125 +1,115 @@
-import '../../../src/App.css';
-import '../../../src/index.css';
-import Bar from '../navbar/navbar';
+import React, { useState } from "react";
 import image from '../../assets/picbuku.png';
-import { navigate, useNavigate } from 'react-router-dom';
-
+import Bar from '../navbar/navbar';
 
 function Halaman_Keranjang() {
-  const navigate = useNavigate()
+  const [cartItems, setCartItems] = useState([
+  { id: 1, name: "Harry Potter: Half Blood Prince", price: 10, quantity: 2, image: "https://imgur.com/c1GmL6Q" },
+  { id: 2, name: "Unbirthday", price: 20, quantity: 1, image: "https://imgur.com/undefined" },
+]);
+
+
+  const handleRemoveItem = (id) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCartItems);
+  };
+  
+
+  const handleQuantityChange = (id, newQuantity) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === id) {
+        return { ...item, quantity: newQuantity };
+      } else {
+        return item;
+      }
+    });
+    setCartItems(updatedCartItems);
+  };
+
+  const subtotal = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
+  const handleCheckout = () => {
+    const checkoutData = {
+      items: cartItems,
+      total: subtotal,
+    };
+
+    // Kirim data ke halaman checkout
+    // navigate('/checkout', { state: checkoutData });
+  };
+
   return (
-    <div className="bg-bg_cream">
-      <Bar/>
+    <div className="bg-bg_cream min-h-screen">
+      <Bar />
+  
+      <div className="flex mt-3 container mx-auto">
+        <div className="mx-4 md:mx-8 py-8 w-full">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden px-4 md:px-10 py-4">
+  
+            <h1 className="text-2xl font-bold mb-10 mt-5">Keranjang Buku</h1>
+            {cartItems.length === 0 ? (
+              <p>Your cart is empty.</p>
+            ) : (
+              <div className="overflow-x-auto">
+                <table className="table-auto w-full mb-4">
+                  <thead>
+                    <tr>
+                      <th className="px-2 md:px-4 py-2">Product</th>
+                      <th className="px-2 md:px-4 py-2">Price</th>
+                      <th className="px-2 md:px-4 py-2">Quantity</th>
+                      <th className="px-2 md:px-4 py-2">Subtotal</th>
+                      <th className="px-2 md:px-4 py-2"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cartItems.map((item) => (
+                      <tr key={item.id}>
+                        <td className="border px-2 md:px-4 py-2">
+                          <div className="flex items-center">
+                            <img src={image} alt={item.name} className="w-16 h-auto mr-4" />
+                            {item.name}
+                          </div>
+                        </td>
 
-      <div class="max-w-7xl mx-8 py-8 flex mt-3">
-        <div class="w-4/5">
-          <div class="bg-white shadow-md rounded-lg overflow-hidden px-10 py-4">
-            <p class="text-sm mb-2 font-bold">KERANJANG BUKU</p>
-              <li className="flex mb-4 -mt-7" style={{ borderBottom: "1px solid #999999", paddingBottom: "1cm" }}></li>
+                        <td className="border px-2 md:px-4 py-2">${item.price}</td>
 
-              <ul>
-                <li className="flex mb-4 mt-7">
-                  <img src={image} style={{ maxWidth: "16%", maxHeight: "16%" }} className="max-w-full h-auto" alt="Book Cover"/>
-                  <div className="ml-10 mt-5">
-                    <p className="text-lg font-bold" style={{ whiteSpace: "nowrap" }}>Harry Potter: Half Blood Prince</p>
-                    <p className="text-sm mt-5">Code Produk : 34528</p>
-                  </div>
-                      
-                  <div className="w-2/4 text-right ml-10 mt-10">
-                    <p className="text-md font-bold">Rp260.000</p>
-                  </div>
-                </li>
-              </ul>
+                        <td className="border px-2 md:px-4 py-2">
+                          <div className="flex items-center">
+                            <button className="bg-gray-200 hover:bg-gray-400 rounded-l px-2 md:px-4 py-1" onClick={() => handleQuantityChange(item.id, item.quantity - 1)}>-</button>
+                            <span className="border-l border-r px-4 py-1">{item.quantity}</span>
+                            <button className="bg-gray-200 hover:bg-gray-400 rounded-r px-2 md:px-4 py-1" onClick={() => handleQuantityChange(item.id, item.quantity + 1)}>+</button>
+                          </div>
+                        </td>
 
-              <ul>
-                <li className="flex mb-4 mt-7">
-                  <img src={image} style={{ maxWidth: "16%", maxHeight: "16%" }} className="max-w-full h-auto" alt="Book Cover"/>
-                  <div className="ml-10 mt-5">
-                    <p className="text-lg font-bold" style={{ whiteSpace: "nowrap" }}>Harry Potter: Half Blood Prince</p>
-                    <p className="text-sm mt-5">Code Produk : 34528</p>
-                  </div>
-                      
-                  <div className="w-2/4 text-right ml-10 mt-10">
-                    <p className="text-md font-bold">Rp260.000</p>
-                  </div>
-                </li>
-              </ul>
+                    <td className="border px-2 md:px-4 py-2">${item.price * item.quantity}</td>
+                    <td className="border px-2 md:px-4 py-2">
 
-              <ul>
-                <li className="flex mb-4 mt-7">
-                  <img src={image} style={{ maxWidth: "16%", maxHeight: "16%" }} className="max-w-full h-auto" alt="Book Cover"/>
-                  <div className="ml-10 mt-5">
-                    <p className="text-lg font-bold" style={{ whiteSpace: "nowrap" }}>Harry Potter: Half Blood Prince</p>
-                    <p className="text-sm mt-5">Code Produk : 34528</p>
-                  </div>
-                      
-                  <div className="w-2/4 text-right ml-10 mt-10">
-                    <p className="text-md font-bold">Rp260.000</p>
-                  </div>
-                </li>
-              </ul>
+                    <button onClick={() => handleRemoveItem(item.id)} className="text-red-500 hover:text-red-800">Remove</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        
+            )}
+            <div className="flex justify-end mt-5">
+              <p className="text-lg font-bold text-gray-800">Subtotal: ${subtotal}</p>
             </div>
-          </div>
-
-        <div class="w-1/3 ml-5">
-          <div class="bg-white shadow-md rounded-lg overflow-hidden px-10 py-4">
-            <p class="text-sm mb-2 font-bold">RINGKASAN PEMESANAN</p>
-            <li className="flex mb-4 -mt-7" style={{ borderBottom: "1px solid #999999", paddingBottom: "1cm" }}></li>
-
-            <ul>
-              <li className="flex mb-4 mt-7">
-                <div className="flex justify-between">
-                  <p className="w-1/3 text-sm text-center">ITEM</p>
-                  <p className="w-1/3 text-sm text-center mx-10">3</p>
-                  <p className="w-1/3 text-sm text-center ml-20" style={{ whiteSpace: "nowrap" }}>RP. 520.000</p>
-                </div>
-              </li>
-
-              <li className="flex mb-4 mt-7">
-                <div className="flex justify-between">
-                  <p className="w-1/3 text-sm text-center">DISCOUNT</p>
-                  <p className="w-1/3 text-sm text-center ml-40" style={{ whiteSpace: "nowrap" }}>20%</p>
-                </div>
-              </li>
-
-              <li className="flex mb-4 mt-7">
-                <div className="flex justify-between">
-                  <p className="w-1/3 text-sm text-center mt-2">PENGIRIMAN</p>
-                </div>
-              </li>
-
-              <li className="flex mb-4">
-                <div className="flex justify-between relative">
-                    <select className="text-sm block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                      <option>Instant Dari Rp.19.000</option>
-                      <option>Same Day Dari Rp.14.000</option>
-                      <option>Reguler Rp.9.000</option>
-                      <option>Hemat Rp.11.000</option>
-                      <option>Kargo Rp.6.000</option>
-                    </select>
-
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                      <svg className="fill-current h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"/></svg>
-                    </div>
-                </div>
-              </li>
-
-              <li className="flex mb-4" style={{ borderBottom: "1px solid #999999", paddingBottom: "1cm" }}></li>
-
-              <li className="flex mb-4 mt-7">
-                <div className="flex justify-between">
-                  <p className="w-1/3 text-sm text-center font-bold">TOTAL</p>
-                  <p className="w-1/3 font-bold text-sm text-center ml-40" style={{ whiteSpace: "nowrap" }}>Rp.427.000</p>
-                </div>
-                
-              </li>
-              <button onClick={() => navigate('/pembayaran')} className="bg-bg_button hover:bg-bg_button_hover tnteext-sm text-white py-2 px-24 ml-2 mt-6 mb-5 rounded">Check Out</button> 
-            </ul> 
+            
+            <div className="flex justify-center mt-10">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleCheckout}>
+                Checkout
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Halaman_Keranjang;
