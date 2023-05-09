8eee2bbfe2334faf94024adb9d50f3d86a46874e
RajendraMaheswara <rajendraagrafm@gmail.com>
2023-05-09 07:40:11 +0700

    pull

--- src/components/halaman_admin/admin.js
+++ src/components/halaman_admin/admin.js
@@ -7,59 +7,51 @@ const Halaman_Admin = () => {
       <div>
         <Bar />
         <div class="container mx-auto p-4">
-      <h1 class="text-3xl font-bold mb-4">Admin Bookstore</h1>
-      <form action="" method="POST">
-        <div class="mb-4">
-          <label for="title" class="block font-bold mb-2">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Enter book title"
-            class="border rounded py-2 px-3 w-full"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="author" class="block font-bold mb-2">Author</label>
-          <input
-            type="text"
-            id="author"
-            name="author"
-            placeholder="Enter book author"
-            class="border rounded py-2 px-3 w-full"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="description" class="block font-bold mb-2"
-            >Description</label
-          >
-          <textarea
-            id="description"
-            name="description"
-            placeholder="Enter book description"
-            class="border rounded py-2 px-3 w-full"
-          ></textarea>
-        </div>
-        <div class="mb-4">
-          <label for="price" class="block font-bold mb-2">Price</label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            placeholder="Enter book price"
-            class="border rounded py-2 px-3 w-full"
-          />
-        </div>
-        <div>
+        <h1 class="text-3xl font-bold mb-4">Admin Bookstore</h1>
+        <form action="" method="POST">
+          <div class="mb-4">
+            <label for="title" class="block font-bold mb-2">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Enter book title"
+              class="border rounded py-2 px-3 w-full"/>
+          </div>
+          <div class="mb-4">
+            <label for="author" class="block font-bold mb-2">Author</label>
+            <input
+              type="text"
+              id="author"
+              name="author"
+              placeholder="Enter book author"
+              class="border rounded py-2 px-3 w-full"/>
+          </div>
+          <div class="mb-4">
+            <label for="description" class="block font-bold mb-2" > Description </label>
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Enter book description"
+              class="border rounded py-2 px-3 w-full"
+            ></textarea>
+          </div>
+          <div class="mb-4">
+            <label for="price" class="block font-bold mb-2">Price</label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Enter book price"
+              class="border rounded py-2 px-3 w-full"/>
+          </div>
+          <div>
           <button
             type="submit"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Add Book
-          </button>
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Add Book </button>
+          </div>
+        </form>
         </div>
-      </form>
-    </div>
 
       </div>
         
--- src/components/halaman_chat/chat.js
+++ src/components/halaman_chat/chat.js
@@ -168,7 +168,7 @@ function Halaman_Chat() {
 
 <div class="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
    <div class="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
-      <div class="relative flex items-center space-x-4 mt-0">
+      <div class="relative flex items-center space-x-4 mt-0" id='akun'>
          <div class="relative">
             <span class="absolute text-green-500 right-0 bottom-0">
                <svg width="20" height="20">
@@ -297,7 +297,7 @@ function Halaman_Chat() {
                </svg>
             </button>
          </span>
-         <input type="text" placeholder="Write your message!" class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3 mb-0"/>
+         <input id='chat' type="text" placeholder="Write your message!" class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3 mb-0"/>
          <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
             <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-gray-600">
